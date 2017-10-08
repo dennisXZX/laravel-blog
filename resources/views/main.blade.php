c0bce616593858a743ff092ee986ec999b5b50be
Dennis Xiao <dennisboys@gmail.com>
2017-10-08 22:32:16 +1100

    first init

---
+++ resources/views/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        @include('partials._head')
+    </head>
+
+    <body>
+        <!-- nav section -->
+        @include('partials._nav')
+
+        <!-- main content -->
+        <div class="container">
+            <!-- page specific content -->
+            @yield('content')
+
+            <!-- footer -->
+            @include('partials._footer')
+        </div>
+
+        <!-- javascript files -->
+        @include('partials._javascript')
+    </body>
+</html>
