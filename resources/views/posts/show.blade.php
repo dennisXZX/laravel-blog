811a8308e69840193db8c3164cea1bafa6422151
Dennis Xiao <dennisboys@gmail.com>
2017-10-22 23:14:43 +1100

    added edit and delete buttons

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,32 @@
 @section('title', ' | View Post')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
-    <p class="lead">{{ $post->body }}</p>
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>
+        </div>
+        <div class="col-md-4">
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Create At:</dt>
+                    <dd>{{ date('j M, Y H:i', strtotime($post->created_at)) }}</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Last Updated:</dt>
+                    <dd>{{ date('j M, Y H:i', strtotime($post->updated_at)) }}</dd>
+                </dl>
+                <hr />
+                <div class="row">
+                    <div class="col-sm-6">
+                        {!! link_to_route('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')); !!}
+                    </div>
+                    <div class="col-sm-6">
+                        {!! link_to_route('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')); !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
